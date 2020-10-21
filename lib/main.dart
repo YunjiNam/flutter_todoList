f98c00a3f5ad2910675d2f7f5839b2016bf4ac75
yunjiNam <yj607252@gmail.com>
2020-10-21 11:37:27 +0900

    add: FutureBuilder api example add

--- lib/main.dart
+++ lib/main.dart
@@ -183,7 +183,96 @@
 // 부분을 먼저 그려주기 위해 사용된다. 만약 FutureBuilder 가 없다면 데이터가 다 받아지기를 기다린 후
 // 화면을 그리거나 데이터가 변함을 setState() 를 통해 바꾸어 주어야 한다
 
+// import 'package:flutter/material.dart';
+//
+// void main() => runApp(MyApp());
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter Demo',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: MyHomePage(title: 'Future Builder Example'),
+//     );
+//   }
+// }
+//
+// class MyHomePage extends StatefulWidget {
+//
+//   MyHomePage({Key key, this.title}) : super(key: key);
+//   final String title;
+//
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
+//
+// class _MyHomePageState extends State<MyHomePage> {
+//
+//   Future<String> _fetch1() async {
+//     await Future.delayed(Duration(seconds: 2));
+//     return 'Call Data';
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(widget.title),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Text(
+//               '이곳은 데이터 상관없이 불려져 오는 곳입니다.',
+//                 style: TextStyle(fontSize: 20),
+//             ),
+//             FutureBuilder(
+//               future: _fetch1(),
+//               builder: (BuildContext context, AsyncSnapshot snapshot) {
+//                 if (snapshot.hasData == false) {
+//                   return CircularProgressIndicator();
+//                 }
+//                 else if (snapshot.hasError) {
+//                   return Padding(
+//                     padding: const EdgeInsets.all(8.0),
+//                     child: Text(
+//                       'Error: ${snapshot.error}',
+//                       style: TextStyle(fontSize: 15),
+//                     ),
+//                   );
+//                 }
+//                 else {
+//                   return Padding(
+//                       padding: const EdgeInsets.all(8.0),
+//                     child: Text(
+//                       snapshot.data.toString(),
+//                       style: TextStyle(fontSize: 15),
+//                     ),
+//                   );
+//                 }
+//               },
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+//}
+
+
+// FutureBuilder api 사용 예제
+// https://eory96study.tistory.com/33?category=821706
+
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import 'models/weather_model.dart';
 
 void main() => runApp(MyApp());
 
@@ -191,11 +280,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Weather Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.orange,
       ),
-      home: MyHomePage(title: 'Future Builder Example'),
+      home: MyHomePage(title: 'Weather Demo Home Page'),
     );
   }
 }
@@ -211,9 +300,27 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  Future<String> _fetch1() async {
-    await Future.delayed(Duration(seconds: 2));
-    return 'Call Data';
+  Future<Weather> getWeather() async {
+    String apiAddr =
+        "https://api.openweathermap.org/data/2.5/weather?q=seoul&appid=apikey를입력해주세요&units=metric";
+    http.Response response;
+    var data1;
+    Weather weather;
+    try {
+      response = await http.get(apiAddr);
+      data1 = json.decode(response.body);
+      weather = Weather(
+        temp: data1['main']['temp'],
+        tempMax: data1['main']['temp_max'],
+        tempMin: data1['main']['temp_min'],
+        weatherMain: data1['weather'][0]['main'],
+        code: data1['weather'][0]['id'],);
+      print(weather.tempMin);
+    } catch (e) {
+      print(e);
+    }
+
+    return weather;
   }
 
   @override
@@ -223,45 +330,38 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              '이곳은 데이터 상관없이 불려져 오는 곳입니다.',
-                style: TextStyle(fontSize: 20),
-            ),
-            FutureBuilder(
-              future: _fetch1(),
-              builder: (BuildContext context, AsyncSnapshot snapshot) {
-                if (snapshot.hasData == false) {
-                  return CircularProgressIndicator();
-                }
-                else if (snapshot.hasError) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Error: ${snapshot.error}',
-                      style: TextStyle(fontSize: 15),
-                    ),
-                  );
-                }
-                else {
-                  return Padding(
-                      padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      snapshot.data.toString(),
-                      style: TextStyle(fontSize: 15),
-                    ),
-                  );
-                }
-              },
-            ),
-          ],
+        child: FutureBuilder(
+          future: getWeather(),
+          builder: (context, AsyncSnapshot<Weather> snapshot) {
+            if (snapshot.hasData == false) {
+              return CircularProgressIndicator();
+            }
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text('현재 온도 : ${snapshot.data.temp.toString()}'),
+                Text('최저 온도 : ${snapshot.data.tempMin.toString()}'),
+                Text('최고 온도 : ${snapshot.data.tempMax.toString()}'),
+                snapshot.data.code == 800
+                    ? Icon(Icons.wb_sunny)
+                    : snapshot.data.code / 100 == 8 ||
+                        snapshot.data.code / 100 == 2
+                      ? Icon(Icons.wb_cloudy)
+                      : snapshot.data.code / 100 == 3 ||
+                          snapshot.data.code / 100 == 5
+                        ? Icon(Icons.beach_access)
+                        : snapshot.data.code / 100 == 6
+                          ? Icon(Icons.ac_unit)
+                          : Icon(Icons.cloud_circle)
+              ],
+            );
+          },
         ),
       ),
     );
   }
 }
+
 
 
 
